568e806309209d40f5250c395abc74ec4e84fbad
milan-bluegrid <milan.paunovic@bluegrid.io>
2025-10-16 17:50:03 +0200

    restructure dotfiles

--- nvim/lua/config/keybinds.lua
+++
@@ -1,8 +0,0 @@
--- set leader key
-vim.g.mapleader = " "
-
--- nvim netrw file finder
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
-
--- clear highlights
-vim.keymap.set("n", "<ESC>", ":noh<CR>", opts)
